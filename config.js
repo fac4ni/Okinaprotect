9e0cac1bbdc4c483e959ba000169de4b207f06fc
fac4ni <167001139+fac4ni@users.noreply.github.com>
2025-03-13 19:48:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     bot: {
-        prefixe: '+', // Préfixe - Le texte avant les commandes
-        buyer: '1251209545129132110', // Buyer
+        prefixe: '-', // Préfixe - Le texte avant les commandes
+        buyer: '1324055543467348019', // Buyer
         couleur: '#6495ED', // Couleur HEX
-        footer: '4Protect', // Dans la commande help ou autre il y'a un texte en bas
+        footer: 'protectokina', // Dans la commande help ou autre il y'a un texte en bas
         maxServer: '100', // Le nombre de serveur maximum que le bot peut rejoindre 
     }
 }
